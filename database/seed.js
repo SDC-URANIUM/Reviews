8805f3f1999db11d5855b9812048fe2ac2d6ace4
Anthony Cella <anthony.cella242@gmail.com>
2022-07-27 15:07:19 -0400

    Got insertion query working

--- database/seed.js
+++ database/seed.js
@@ -1,16 +1,59 @@
 const extract = require('./extract-data.js');
 const pool = require('./db.js');
 
-const seed = function(exampleData) {
-  console.log(JSON.stringify(exampleData))
+const seed = function (exampleData) {
+  useDatabase();
+  // for (const entry of exampleData) {
+  //   insertIntoDb(entry);
+  // }
 }
 
-const useDatabase = function() {
-  pool.query('SELECT * FROM example', (error, result) => {
+const insertIntoDb = function (entry, databaseName) {
+  const keys = Object.keys(entry);
+  const columns = getColumns(keys);
+
+  const values = Object.values(entry);
+  const tableValues = getValues(values);
+
+  const insertionQuery = 'INSERT INTO example ' + columns + ' VALUES ' + tableValues;
+  pool.query(insertionQuery, (error, result) => {
     if (error) console.log(error);
-    else console.log(JSON.stringify(result.rows));
+    else console.log(JSON.stringify(result));
   })
 }
 
-useDatabase();
-// extract.readExampleCSV(seed);
+const getColumns = function (keys) {
+  let columnConstructor = '(';
+  for (key of keys) {
+    columnConstructor += key + ', ';
+  }
+
+  const columns = columnConstructor.slice(0, columnConstructor.length - 2) + ')';
+  return columns;
+}
+
+const getValues = function (keys) {
+  let valueConstructor = '(';
+  for (key of keys) {
+    valueConstructor += '\'' + key + '\'' + ', ';
+  }
+
+  const values = valueConstructor.slice(0, valueConstructor.length - 2) + ')';
+  return values;
+}
+
+const useDatabase = function () {
+  pool.query('SELECT * FROM example', (error, result) => {
+    if (error) console.log(error);
+    else {
+      for (const row of result.rows) {
+        console.log(JSON.stringify(row));
+      }
+    }
+
+    pool.end();
+  })
+}
+
+// useDatabase();
+extract.readExampleCSV(seed);
