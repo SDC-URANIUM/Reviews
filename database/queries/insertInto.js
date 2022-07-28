e763aa8d149b33a781e9db48c0ef7a57f375881c
Anthony Cella <anthony.cella242@gmail.com>
2022-07-28 11:27:01 -0400

    Got all of my insertion queries done

--- database/queries/insertInto.js
+++ database/queries/insertInto.js
@@ -10,6 +10,84 @@ const example = async function(columns, values) {
   })
 }
 
+const reviewsInfoTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO reviewsinfo \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const metaTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO meta \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const reviewTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO review \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const photosTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO photos \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const ratingsTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO ratings \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const recommendationsTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO recommendations \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+const characteristicsTable = async function(columns, values) {
+  const insertionQuery = "INSERT INTO characteristics \
+    " + columns + " VALUES " + values;
+
+  pool.query(insertionQuery, (error, result) => {
+    if (error) console.log(error);
+    else console.log(result);
+  })
+}
+
+
 module.exports = {
-  example
+  example,
+  reviewsInfoTable,
+  metaTable,
+  reviewTable,
+  photosTable,
+  ratingsTable,
+  recommendationsTable,
+  characteristicsTable,
 }
